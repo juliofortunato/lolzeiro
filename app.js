01b3d96ddc8969443a604dccaca8fc3751688e71
Júlio Fortunato <hi@juliofortunato.dev>
2019-09-28 18:39:40 -0300

    Starter commands

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js')
+const { prefix, discordToken } = require('./config.json')
+
+const client = new Discord.Client()
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`)
+})
+
+client.on('message', message => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return
+
+  const args = message.content.slice(prefix.length).split(/ +/)
+  const command = args.shift().toLowerCase()
+
+  if (command === 'ping') {
+    message.channel.send('Pong.')
+  } else if (command === 'beep') {
+    message.channel.send('Boop.')
+  }
+  // other commands..bundleRenderer.renderToString
+})
+
+client.login(discordToken)
