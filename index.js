fd869819b76256333eafeffd5542a7d2433cbafd
Júlio Fortunato <hi@juliofortunato.dev>
2019-09-29 13:41:24 -0300

    Add lol commands

---
+++ index.js
@@ -0,0 +1,39 @@
+const fs = require('fs')
+const Discord = require('discord.js')
+const { prefix, discordToken } = require('./config.json')
+
+const client = new Discord.Client()
+client.commands = new Discord.Collection()
+
+const commandFiles = fs
+  .readdirSync('./commands')
+  .filter(file => file.endsWith('.js'))
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`)
+})
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`)
+  client.commands.set(command.name, command)
+}
+
+client.on('message', message => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return
+
+  const args = message.content.slice(prefix.length).split(/ +/)
+  const commandName = args.shift().toLowerCase()
+
+  if (!client.commands.has(commandName)) return
+
+  const command = client.commands.get(commandName)
+
+  try {
+    command.execute(message, args)
+  } catch (error) {
+    console.error(error)
+    message.reply('There was an error trying to execute that command!')
+  }
+})
+
+client.login(discordToken)
